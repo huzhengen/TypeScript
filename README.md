38556bc8489875420ae26241b63b15cedac7e5eb
卢方 <huzhengen@163.com>
2019-03-04 16:01:38 +0800

    greeter.ts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TypeScript
+
+F:\nodejs\ts-node -> F:\nodejs\node_modules\ts-node\dist\bin.js
---
+++ tsdemo1/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person){
+    return 'hello ' +  person
+}
+
+let user = 'john'
+
+console.log(greeter(user))

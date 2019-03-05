0cb0d21bb48c2215d60b1bdd41a7a4949981186f
卢方 <huzhengen@163.com>
2019-03-05 14:22:45 +0800

    devide.ts

---
+++ tsdemo2/devide.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env ts-node
+{
+    let a: number = parseInt(process.argv[2])
+    let b: number = parseInt(process.argv[3])
+
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+        console.log('您输入的可能不是整数')
+        process.exit(1)
+    }
+    if (b === 0) {
+        console.log('除数不能为0')
+        process.exit(2)
+    }
+    console.log(a / b)
+    process.exit(0)
+}

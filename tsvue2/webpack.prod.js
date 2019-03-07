91117a3e5c82704b6c5eab4a8ac46997ec475aaa
卢方 <huzhengen@163.com>
2019-03-07 17:57:02 +0800

    webpack-dev-server html-webpack-plugin

--- tsvue2/webpack.prod.js
+++ tsvue2/webpack.prod.js
@@ -1,15 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-    ]
+    }
 };

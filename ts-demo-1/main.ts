f10ec476a128188a3cdaac934b6b9598c46096a9
卢方 <huzhengen@163.com>
2019-07-14 15:16:28 +0800

    拖拽div

---
+++ ts-demo-1/main.ts
@@ -0,0 +1,38 @@
+let div = document.createElement('div')
+div.style.border = '1px solid red'
+div.style.height = '100px'
+div.style.width = '100px'
+div.id = 'demo'
+
+document.body.appendChild(div)
+
+let isMove = false
+let position = [0, 0]
+
+div.onmousedown = (e) => {
+    isMove = true
+    position = [e.clientX, e.clientY]
+}
+console.log(div.style.top)
+
+document.onmousemove = (e) => {
+    console.log(isMove)
+    if (isMove) {
+        let deltaX = e.clientX - position[0]
+        let deltaY = e.clientY - position[1]
+        // div.style.left = e.clientX + 'px'
+        // div.style.top = e.clientX + 'px'
+        console.log(div.style.top)
+        let top = parseInt(div.style.top!) || 0
+        let left = parseInt(div.style.left!) || 0
+        console.log(top, deltaY)
+        div.style.top = top + deltaY + 'px'
+        div.style.left = left + deltaX + 'px'
+        position = [e.clientX, e.clientY]
+    }
+
+}
+
+document.onmouseup = (e) => {
+    isMove = false
+}
